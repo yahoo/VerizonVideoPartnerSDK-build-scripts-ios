46d75fc200a48d845095f492f099a379760e85b6
Andrey Moskvin <andrey.moskvin@teamaol.com>
2017-08-23 14:39:50 +0300

    Added github_changelog_generator action

---
+++ github_changelog_generator.rb
@@ -0,0 +1,85 @@
+module Fastlane
+  module Actions
+    module SharedValues
+      GITHUB_CHANGELOG = :GITHUB_CHANGELOG
+    end
+
+    class GithubChangelogGeneratorAction < Action
+      def self.run(params)        
+        output_file_path = params[:output]
+        UI.message "Parameter GITHUB_CHANGELOG_TOKEN: #{params[:token]}"
+        UI.message "Parameter output: #{output_file_path}"
+        UI.message "Parameter unreleased_only: #{params[:unreleased_only]}"
+
+        action = "github_changelog_generator"
+        action << " --token #{params[:token]}"
+        action << " --output #{output_file_path}" 
+        action << " --unreleased-only" if params[:unreleased_only] == true
+
+        sh action        
+        contents = File.open(output_file_path, 'rb') { |f| f.read }
+        result = contents[/\*{2}.*\)\)/m, 0]
+
+        if result != nil
+          UI.important result
+          result
+        else
+          result = "Zero merged pull requests! Work harder!"
+          UI.important result
+        end
+
+        File.delete(output_file_path)
+        result
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Generate github changelog!"
+      end
+
+      def self.details
+        "Details available here: https://github.com/skywinder/github-changelog-generatorYou can use this action to do cool things..."
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :token,
+                                       env_name: "GITHUB_CHANGELOG_TOKEN", 
+                                       description: "GITHUB_CHANGELOG_TOKEN for Github ChangeLog Generator", #
+                                       verify_block: proc do |value|
+                                          UI.user_error!("No GITHUB_API_TOKEN for Github ChangeLog Generator") unless (value and not value.empty?)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :output,
+                                       description: "Output file path + name for changelog",
+                                       is_string: true,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :unreleased_only,
+                                       description: "Unreleased Changelog only or not",
+                                       is_string: false, 
+                                       default_value: false)
+        ]
+      end
+
+      def self.output
+        [
+          ['GITHUB_CHANGELOG', 'A changelog generated from GitHub']
+        ]
+      end
+
+      def self.return_value
+        "String with a changelog text."
+      end
+
+      def self.authors
+        ["AndriiMoskvin/Berk0ld"]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
