ebd47733d040a8af7e93951d61e8f6bde799cf72
Andrey Moskvin <andrey.moskvin@teamaol.com>
2017-10-06 15:59:46 +0300

    Added generate json file fastlane action

---
+++ json/generate_json_file.rb
@@ -0,0 +1,47 @@
+module Fastlane
+  module Actions
+
+    class GenerateJsonFileAction < Action
+      def self.run(params)        
+        destination = params[:destination]
+        File.open(destination, 'w') do |file|
+          require 'json'
+          json = params[:json]
+          
+          file.write(json.to_json)
+        end
+      end
+
+      #####################################################
+      # @!group Documentation
+      #####################################################
+
+      def self.description
+        "Writes provided json to destination file"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :json,
+                                       description: "JSON compatible hash",
+                                       is_string: false,
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :destination,
+                                       description: "Destination for json file",
+                                       optional: false,
+                                       verify_block: proc do |value| 
+                                          # add verification logic here
+                                       end)
+        ]
+      end
+
+      def self.authors
+        ["AndriiMoskvin"]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
